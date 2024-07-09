b92fa19e5f158e1e8018c932c3c2c501e2c3d770
qiqinghua <qiqinghua@hotmail.com>
2024-07-09 20:08:10 +0800

    microblog的第一个版本

---
+++ microblog.py
@@ -0,0 +1,6 @@
+from app import app
+
+# 'from app import app 中的第一个 app 是指 app 模块（通常是一个目录）。
+# 第二个 app 是指在 app 模块中定义的 Flask 应用实例变量 app。
+# 注意，要加上 export FLASK_APP=microblog.py，
+# windows 下是set命令，这样flask run就是执行microblog.py这个文件了
