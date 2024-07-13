5a5c5e6e042c8d8f63e2e14b3579fe24ddf9d0b9
qiqinghua <qiqinghua@hotmail.com>
2024-07-13 21:54:13 +0800

    userlogin_and_register

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,33 @@
-from flask_wtf import FlaskForm #导入FlaskForm基类，用于定义表单。
-from wtforms import StringField, PasswordField, BooleanField, SubmitField #导入WTForms中的表单字段类。
-from wtforms.validators import DataRequired #导入一个验证器，用于验证字段是否为空。
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+import sqlalchemy as sa
+from app import db
+from app.models import User
+
 
 class LoginForm(FlaskForm): #定义一个继承自FlaskForm的表单类
     username = StringField('Username', validators=[DataRequired()]) #定义一个名为username的字符串字段，并添加DataRequired验证器，确保该字段不能为空。
     password = PasswordField('Password', validators=[DataRequired()]) #定义一个名为password的密码字段，并添加DataRequired验证器。
     remember_me = BooleanField('Remember Me') #定义一个名为remember_me的布尔字段，用于“记住我”选项。
     submit = SubmitField('Sign In') # 定义一个提交按钮
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = db.session.scalar(sa.select(User).where(
+            User.username == username.data))
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = db.session.scalar(sa.select(User).where(
+            User.email == email.data))
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
--- microblog.py
+++ microblog.py
@@ -7,6 +7,8 @@ from app.models import User, Post
 def make_shell_context():
     return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
 
+
+
 # 'from app import app 中的第一个 app 是指 app 模块（通常是一个目录）。
 # 第二个 app 是指在 app 模块中定义的 Flask 应用实例变量 app。
 # 注意，要加上 export FLASK_APP=microblog.py，
